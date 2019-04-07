d8c2f50ae76f89717916f5b01c6afb39f5024c6b
Midona <hauxhaux@gmail.com>
2019-04-07 13:47:19 +0200

    prettified test setup

--- app/parkit/__test__/setup.ts
+++ app/parkit/__test__/setup.ts
@@ -1,5 +1,7 @@
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import { GlobalWithFetchMock } from "jest-fetch-mock";
 
-(global as any).fetch = require("jest-fetch-mock");
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetchMock = customGlobal.fetch;
 Enzyme.configure({ adapter: new Adapter() });
